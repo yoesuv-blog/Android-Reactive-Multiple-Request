d01cdbd93382fc75b64fcb7b9ce049dda8659bb5
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-03-06 10:29:51 +0700

    :construction: setup activity

---
+++ app/src/main/java/com/yoesuv/multiplerequest/menu/viewmodels/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.yoesuv.multiplerequest.menu.viewmodels
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+}
--- app/src/main/java/com/yoesuv/multiplerequest/menu/views/MainActivity.kt
+++ app/src/main/java/com/yoesuv/multiplerequest/menu/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.yoesuv.multiplerequest
+package com.yoesuv.multiplerequest.menu.views
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.yoesuv.multiplerequest.R
 
 class MainActivity : AppCompatActivity() {
 
